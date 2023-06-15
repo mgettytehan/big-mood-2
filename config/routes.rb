f95d11690e8cf7a9ca76a687adecf75d61f2cbfd
Marion Getty-Tehan <mgettytehan@gmail.com>
2023-06-15 09:56:21 -0400

    All resource routes added to API namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :mood_logs
-  resources :scale_items
-  resources :moodscales
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    resources :mood_logs
+    resources :scale_items
+    resources :moodscales
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
